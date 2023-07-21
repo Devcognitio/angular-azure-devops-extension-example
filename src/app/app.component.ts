09a391b6d06b09db5fd4353ec5745dd7fed56950
Antony Jose Triana De La Hoz <antony.triana@banistmo.com>
2023-07-21 13:56:08 -0500

    feat: Add SDK init and initializeState function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as SDK from "azure-devops-extension-sdk";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-extension-angular';
+  userName: string | undefined;
+
+  ngOnInit() {
+    SDK.init();
+    this.initializeState();
+  }
+
+  private async initializeState(): Promise<void> {
+    await SDK.ready();
+    this.userName = SDK.getUser().displayName;
+  }
+
 }
